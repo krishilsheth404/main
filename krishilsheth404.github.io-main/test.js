7956fd9aab62de38e44598d570cdfd1a3d501e04
krishil sheth <krishisheth18@.com>
2022-01-18 22:22:50 +0530

    working on tata 1mg

--- krishilsheth404.github.io-main/test.js
+++ krishilsheth404.github.io-main/test.js
@@ -187,10 +187,10 @@ app.post('/result', async(req, res) => {
 
             return {
                 name: 'Tata 1mg',
-                item: $('.container-fluid-padded>h1').text() == "" ? $('.style__pro-title___3zxNC').first().text() : $('.container-fluid-padded>h1').text(),
+                item: $('.container-fluid-padded>h1').text() == "" ? $('.style__pro-title___3G3rr').first().text() : $('.container-fluid-padded>h1').text(),
                 // item: item,
                 // price: $('.DrugPriceBox__price___dj2lv').text(),
-                price: $('.Price__price__22Jxo').text() == "" ? $('.style__pack-size___254Cd').first().text() + " " + $('.style__product-pricing___1tj_E').first().text() : $('.Price__price__22Jxo').text(),
+                price: $('.Price__price__22Jxo').text() == "" ? $('.style__price-tag___KzOkY').first().text() + " " + $('.style__product-pricing___1tj_E').first().text() : $('.Price__price__22Jxo').text(),
             };
 
         } catch (error) {
